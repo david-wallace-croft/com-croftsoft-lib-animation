d5288d24af0b418888114637ffb007e51752de7e
David Wallace Croft <david@CroftSoft.com>
2025-01-14 22:03:04 -0600

    replace deprecated

--- src/painter/background/mod.rs
+++ src/painter/background/mod.rs
@@ -2,10 +2,10 @@
 //! - Background Painter for the CroftSoft Animation Library
 //!
 //! # Metadata
-//! - Copyright: &copy; 2023 [`CroftSoft Inc`]
+//! - Copyright: &copy; 2023-2025 [`CroftSoft Inc`]
 //! - Author: [`David Wallace Croft`]
 //! - Created: 2023-03-03
-//! - Updated: 2023-09-02
+//! - Updated: 2025-01-14
 //!
 //! [`CroftSoft Inc`]: https://www.croftsoft.com/
 //! [`David Wallace Croft`]: https://www.croftsoft.com/people/david/
@@ -14,14 +14,13 @@
 use com_croftsoft_lib_role::Painter;
 use core::cell::RefCell;
 use std::rc::Rc;
-use wasm_bindgen::JsValue;
 use web_sys::CanvasRenderingContext2d;
 
 pub struct BackgroundPainter {
   canvas_height: f64,
   canvas_width: f64,
   context: Rc<RefCell<CanvasRenderingContext2d>>,
-  fill_style: JsValue,
+  fill_style: String,
 }
 
 impl BackgroundPainter {
@@ -31,7 +30,8 @@ impl BackgroundPainter {
     context: Rc<RefCell<CanvasRenderingContext2d>>,
     fill_style_str: &str,
   ) -> Self {
-    let fill_style: JsValue = JsValue::from_str(fill_style_str);
+    let fill_style: String = fill_style_str.to_string();
+
     Self {
       canvas_height,
       canvas_width,
@@ -44,7 +44,9 @@ impl BackgroundPainter {
 impl Painter for BackgroundPainter {
   fn paint(&self) {
     let context = self.context.borrow();
-    context.set_fill_style(&self.fill_style);
+
+    context.set_fill_style_str(&self.fill_style);
+
     context.fill_rect(0., 0., self.canvas_width, self.canvas_height);
   }
 }
